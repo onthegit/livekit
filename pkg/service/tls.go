45c6d6dc48c27275f9543d4722ea33213c9118fd
linux <linux@localhost.localdomain>
2023-07-10 00:28:19 +0200

    Update

--- pkg/service/tls.go
+++ pkg/service/tls.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 )
 
-// getListenerFromConfig
+// getListenerFromConfig will create TLS listener if TLS config is available.
 func (s *LivekitServer) getListenerFromConfig(addr string) (net.Listener, error) {
 	if s.config.TLS != nil {
 		return tls.Listen("tcp", net.JoinHostPort(addr, strconv.Itoa(int(s.config.Port))), s.config.TLS)
